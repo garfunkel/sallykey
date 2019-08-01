5e11bc9d0773bef6a43fd4c15e8650f447c51dda
Simon Allen <simon@simonallen.org>
2019-08-01 20:53:29 +1000

    initial commit

---
+++ sallykey.go
@@ -0,0 +1,105 @@
+/*
+ * Easy to use, cross platform tool for generating public/private SSH key pairs.
+ */
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+)
+
+const (
+	PRIVATE_KEY_FILE = "id_rsa"
+	PUBLIC_KEY_FILE  = "id_rsa.pub"
+	BITS             = 2048
+	PRE_DESCRIPTION  = "This tool can be used to generate an SSH public/private key pair." +
+		"\nKeys are generated using the RSA algorithm and have are 2048 bits in length." +
+		"\n\nThe resulting key pair can be imported for use in programs" +
+		"\nsuch as FileZilla, as well as from the command line."
+	POST_DESCRIPTION = "Key pair has been generated successfully." +
+		"\nPlease find the files below in the same directory as this program:" +
+		"\n\nPrivate key: " + PRIVATE_KEY_FILE +
+		"\nPublic key: " + PUBLIC_KEY_FILE
+	ERROR_DESCRIPTION = "Error generating key pair." +
+		"\nPlease find error details below:" +
+		"\n\n"
+)
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("SSH Key Generator")
+	description := widget.NewLabel(PRE_DESCRIPTION)
+	generate := widget.NewButton("Generate", nil)
+
+	generate.OnTapped = func() {
+		err := generateKeyPair()
+
+		if err == nil {
+			description.SetText(POST_DESCRIPTION)
+		} else {
+			description.SetText(ERROR_DESCRIPTION + err.Error())
+		}
+
+		generate.Disable()
+	}
+
+	window.SetContent(widget.NewVBox(
+		description,
+		widget.NewHBox(
+			layout.NewSpacer(),
+			generate,
+			widget.NewButton("Close", func() {
+				app.Quit()
+			}),
+			layout.NewSpacer(),
+		),
+	))
+
+	window.SetFixedSize(true)
+	window.CenterOnScreen()
+	window.ShowAndRun()
+}
+
+func generateKeyPair() error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, BITS)
+
+	if err != nil {
+		return err
+	}
+
+	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
+
+	if err != nil {
+		return err
+	}
+
+	privateKeyText := encodePrivateKeyToPEM(privateKey)
+	publicKeyText := ssh.MarshalAuthorizedKey(publicKey)
+
+	err = ioutil.WriteFile(PRIVATE_KEY_FILE, privateKeyText, 0600)
+
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(PUBLIC_KEY_FILE, publicKeyText, 0600)
+
+	return err
+}
+
+func encodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+	privateBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	return pem.EncodeToMemory(&privateBlock)
+}
